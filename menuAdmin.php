20054339357a4614d6132e00a99c7a72954c8378
Cyc87 <chenalcyril87@gmail.com>
2019-04-24 11:32:42 +0200

    include menu in admin.php

---
+++ menuAdmin.php
@@ -0,0 +1,28 @@
+<header>
+            <nav id="navbar" class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a class="navbar-brand" id="navbar-brand" href="index.php">Jean Forteroche</a>
+                <button id="button" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span id="span" class="navbar-toggler-icon "></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto " id="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" id="nav-link" href="creation_article.php">Créer chapitre</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="nav-link" href="modification_article.php">Modifier</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="nav-link" href="suppression_article.php">Supprimer</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="nav-link" href="account.php">Créer compte</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="nav-link" href="deconnexion.php">Se déconnecter</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
