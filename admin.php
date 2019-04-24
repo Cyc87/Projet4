9784eaa3fb4c068be3dfcc256d0ee61a4c5d210d
Cyc87 <chenalcyril87@gmail.com>
2019-04-24 11:50:02 +0200

    Changement admin.php by index.php

--- admin.php
+++ admin.php
@@ -7,7 +7,6 @@
         exit();
     }
    
-    
     try {
         $bdd = new PDO('mysql:host=localhost;dbname=projet4;charset=utf8', 'root', '');
     } catch (Exception $e) {
@@ -37,5 +36,4 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         </body>
-
 </html> 
