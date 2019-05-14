cade36476f695d0a1f84b9cfda80f26dd607eee3
Cyc87 <chenalcyril87@gmail.com>
2019-05-14 10:17:06 +0200

    nothing

--- admin.php
+++ admin.php
@@ -19,6 +19,7 @@
     $data = $req->fetch(PDO::FETCH_ASSOC);
 
     
+    
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -34,6 +35,7 @@
     </head>
        <?php include("menuAdmin.php"); ?>
     <body>
+
         
             
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
