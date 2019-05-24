80d81096f9bfdcdc03ffa2cc70716e3d2508ff92
Cyc87 <chenalcyril87@gmail.com>
2019-05-24 11:46:39 +0200

    nothing

--- menuAdmin.php
+++ menuAdmin.php
@@ -1,28 +1,28 @@
 <header>
-            <nav id="navbar" class="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a class="navbar-brand" id="navbar-brand" href="index.php">Jean Forteroche</a>
-                <button id="button" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span id="span" class="navbar-toggler-icon "></span>
-                </button>
+    <nav id="navbar" class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" id="navbar-brand" href="index.php">Jean Forteroche</a>
+        <button id="button" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span id="span" class="navbar-toggler-icon "></span>
+        </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto " id="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" id="nav-link" href="creationChapter.php">Créer chapitre</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" id="nav-link" href="modificationChapter.php">Modifier</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" id="nav-link" href="deleteChapter.php">Supprimer</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" id="nav-link" href="creationAccount.php">Créer compte</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" id="nav-link" href="deconnexion.php">Se déconnecter</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto " id="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" id="nav-link" href="creationChapter.php">Créer chapitre</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="nav-link" href="modificationChapter.php">Modifier</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="nav-link" href="deleteChapter.php">Supprimer</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="nav-link" href="creationAccount.php">Créer compte</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="nav-link" href="deconnexion.php">Se déconnecter</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
