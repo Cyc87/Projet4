4ef8eda21a6885ec54dd983440255f6ca8dc72d4
Cyc87 <chenalcyril87@gmail.com>
2019-05-24 11:48:15 +0200

    set up Chapter and ChapterManager for the POO

---
+++ Chapter.php
@@ -0,0 +1,64 @@
+<?php 
+
+    class ChapterCreation{
+        
+        private $_id;
+        private $_numberChapter;
+        private $_titleChater;
+        private $_contentChater;
+        private $_dateCreationChapter;
+
+        public function __construct(Array $data){
+            $this->hydrate($data);
+        }
+        public function hydrate(Array $data){
+            if(isset($data['id'])){
+                $this->setId($data['id']);
+            }
+            if(isset($data['numberChapter'])){
+                $this->setNumberChapter($data['numberChapter']);
+            }
+            if(isset($data['titleChapter'])){
+                $this->setTitleChapter($data['titleChapter']);
+            }
+            if(isset($data['contentChapter'])){
+                $this->setContentChapter($data['contentChapter']);
+            }
+            if(isset($data['dateCreationChapter'])){
+                $this->setDateCreationChapter($data['dateCreattionChapter']);
+            }
+        }
+        // GETTERS
+        public function id(){
+            return $this->_id;
+        }
+        public function numberChapter(){
+            return $this->_numberChapter;
+        }
+        public function titleChapter(){
+            return $this->_titleChapter;
+        }
+        public function contentChapter(){
+            return $this->_contentChapter;
+        }
+        public function dateCreationChapter(){
+            return $this->_dateCreationChapter;
+        }
+        // SETTERS
+        public function setId($id){
+            $this->_id = $id;
+        }
+        public function setNumberChapter($numberChapter){
+            $this->_numberChapter = $numberChapter;
+        }
+        public function setTitleChapter($titleChapter){
+            $this->_titleChapter = $titleChapter;
+        }
+        public function setContentChapter($contentChapter){
+            $this->_contentChapter = $contentChapter;
+        }
+        public function setDateCreationChapter($dateCreationChapter){
+            $this->_dateCreationChapter = $dateCreationChapter;
+        }
+    }
+?>
---
+++ ChapterManager.php
@@ -0,0 +1,54 @@
+<?php
+
+    class ChapterManager{
+
+        private $_db;
+
+        public function __construct(){
+            try {
+                $this->_db = new PDO('mysql:host=localhost;dbname=projet4;charset=utf8', 'root', '');
+            } catch (Exception $e) {
+                die('Erreur : ' . $e->getMessage());
+            }
+        }
+        public function getByContentChapter($contentChapter){
+            $req = $this->_db->prepare("SELECT * FROM `chapter` WHERE contentChapter = :contentChapter");
+            $req->execute(array(
+                "contentChapter" => $contentChapter
+            ));
+            $data = $req ->fetch(PDO::FETCH_ASSOC);
+            return $data;
+        }
+        public function getByTitleChapter($titleChapter){
+            $req = $this->_db->prepare("SELECT * FROM `chapter` WHERE titleChapter = :titleChapter");
+            $req->execute(array(
+                "titleChapter" => $titleChapter
+            ));
+            $data = $req ->fetch(PDO::FETCH_ASSOC);
+            return $data;
+        }
+        public function getByNumberChapter($numberChapter){
+            $req = $this->_db->prepare("SELECT * FROM `chapter` WHERE numberChapter = :numberChapter");
+            $req->execute(array(
+                "numberChapter" => $numberChapter
+            ));
+            $data = $req ->fetch(PDO::FETCH_ASSOC);
+            return $data;
+        }
+        public function addChapterCreation(ChapterCreation $chapter){
+            $req = $this->_db->prepare("INSERT INTO chapter(numberChapter, titleChapter, contentChapter, dateCreationChapter) VALUES (?,?,?,NOW())");
+            $req->execute(array(
+                $chapter->numberChapter(),
+                $chapter->titleChapter(),
+                $chapter->contentChapter(),
+            ));
+        }
+        public function deleteChapterCreation(ChapterCreation $chapter){
+            $req = $this->_db->prepare("DELETE FROM chapter WHERE id = :id");
+            $req->execute(array(
+                $chapter->id(),
+            ));
+        }
+    }
+
+?>
